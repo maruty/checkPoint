c2b951f6e85fe51aaf03fd9df543e6e5bcaf1f07
柳澤 雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2018-07-08 14:21:16 +0900

    持ち株

--- src/com/blog/marublo/PointSaveBatch.java
+++ src/com/blog/marublo/PointSaveBatch.java
@@ -83,7 +83,7 @@ public class PointSaveBatch extends AbstractPointController{
 		System.out.println(driver.findElement(By.xpath("//*[@id=\"content\"]/table[2]/tbody/tr[4]/td")).getText());
 		String kabusu[] = driver.findElement(By.xpath("//*[@id=\"content\"]/table[2]/tbody/tr[4]/td")).getText().split(".");
 		//,までをいったん取得
-		String kakaku[] = driver.findElement(By.cssSelector("#content > table.tblbasic > tbody > tr:nth-child(7) > td")).getText().split(",");
+		String kakaku[] = driver.findElement(By.xpath("//*[@id=\"content\"]/table[2]/tbody/tr[7]/td")).getText().split(",");
 		String kakakuMaster = kakaku[0] + kakaku[1].substring(1, 3);
 
 		int goukeiKingaku = Integer.parseInt(kabusu[0]) * Integer.parseInt(kakakuMaster);
